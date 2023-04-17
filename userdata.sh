aebc76e10ce3726726abd479694bd7d079569d86
Yasin Cetindil <ycetindil@gmail.com>
2023-04-17 12:25:47 -0700

    Change Jenkins GPG key

--- userdata.sh
+++ userdata.sh
@@ -7,7 +7,7 @@ yum install git -y
 
 # Install Jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade
 amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
